db1c12281f2de6d9cba3ebf21f159b27d5f341a0
Jake Krog <jake.krog@mac.com>
2018-02-28 02:29:22 -0800

    Bump version

--- bme280_exporter/__about__.py
+++ bme280_exporter/__about__.py
@@ -5,7 +5,7 @@ __title__ = "bme280_exporter"
 __summary__ = "Prometheus exporter for the Bosh BME280 sensor"
 __uri__ = "https://github.com/ashtreefarms/bme280_exporter"
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 __author__ = "Jake Krog"
 __email__ = "jake.krog@gmail.com"
